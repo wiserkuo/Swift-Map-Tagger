f59b1ea4e83138940ed76abce98d02d60b3793e2
wiserkuo <wiserkuo@gmail.com>
2015-02-06 15:32:02 +0800

    Initial Commit

---
+++ Map Tagger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Map Tagger
+//
+//  Created by Wiser Kuo on 2015/2/6.
+//  Copyright (c) 2015年 sw5. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
