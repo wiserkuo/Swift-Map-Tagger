63a4c86a8a0ae84b1606d18be150825afaf06ea2
wiserkuo <wiserkuo@gmail.com>
2015-03-11 14:18:09 +0800

    Add JSON,Search Place,Basic Marker

---
+++ Map Tagger/DataManager.swift
@@ -0,0 +1,123 @@
+//
+//  DataManager.swift
+//  Map Tagger
+//
+//  Created by Wiser Kuo on 2015/3/3.
+//  Copyright (c) 2015年 sw5. All rights reserved.
+//
+
+import Foundation
+let TopAppURL = "https://itunes.apple.com/us/rss/topgrossingipadapplications/limit=25/json"
+let GMSURL = "https://maps.googleapis.com/maps/api/geocode/json?address="
+class DataManager {
+    
+    class func getTopAppsDataFromFileWithSuccess(success: ((data: NSData) -> Void)) {
+        //1
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+            //2
+            let filePath = NSBundle.mainBundle().pathForResource("TopApps",ofType:"json")
+            
+            var readError:NSError?
+            if let data = NSData(contentsOfFile:filePath!,
+                options: NSDataReadingOptions.DataReadingUncached,
+                error:&readError) {
+                    success(data: data)
+            }
+        })
+    }
+    
+    class func loadDataFromURL(url: NSURL, completion:(data: NSData?, error: NSError?) -> Void) {
+        var session = NSURLSession.sharedSession()
+        
+        // Use NSURLSession to get data from an NSURL
+        let loadDataTask = session.dataTaskWithURL(url, completionHandler: { (data: NSData!, response: NSURLResponse!, error: NSError!) -> Void in
+            if let responseError = error {
+                completion(data: nil, error: responseError)
+            } else if let httpResponse = response as? NSHTTPURLResponse {
+                if httpResponse.statusCode != 200 {
+                    var statusError = NSError(domain:"com.raywenderlich", code:httpResponse.statusCode, userInfo:[NSLocalizedDescriptionKey : "HTTP status code has unexpected value."])
+                    completion(data: nil, error: statusError)
+                } else {
+                    completion(data: data, error: nil)
+                }
+            }
+        })
+        
+        loadDataTask.resume()
+    }
+    class func getTopAppsDataFromItunesWithSuccess(success: ((iTunesData: NSData!) -> Void)) {
+        //1
+        loadDataFromURL(NSURL(string: TopAppURL)!, completion:{(data, error) -> Void in
+            //2
+            if let urlData = data {
+                //3
+                success(iTunesData: urlData)
+            }
+        })
+    }
+    class func getCoordinateFromGMS(address: String ) -> CLLocationCoordinate2D {
+        var latitude: Double!
+        var longtitude: Double!
+        var formatted_address: String!
+        var data: NSData!
+        //1
+        /*loadDataFromURL(NSURL(string: GMSURL+address.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!)!, completion:{(data, error) -> Void in
+            println("URL:\(GMSURL)\(address)")
+            //2
+            if let urlData = data {
+              //3
+              success(GMSData: urlData)
+            }
+            let json = JSON(data: data!)
+            if let lat = json["results"][0]["geometry"]["location"]["lat"].double {
+                print("in getCoordinate:\(lat),")
+                latitude=lat
+            }
+            if let lng = json["results"][0]["geometry"]["location"]["lng"].double {
+                println("\(lng)")
+                longtitude=lng
+            }
+            println("latitude=\(latitude) , longtitude=\(longtitude)")
+            
+            
+        })*/
+        // Use NSURLSession to get data from an NSURL
+       /* var session = NSURLSession.sharedSession()
+        let loadDataTask = session.dataTaskWithURL(NSURL(string: GMSURL+address.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!)!, completionHandler: { (data: NSData!, response: NSURLResponse!, error: NSError!) -> Void in
+            if let responseError = error {
+               // completion(data: nil, error: responseError)
+            } else if let httpResponse = response as? NSHTTPURLResponse {
+                if httpResponse.statusCode != 200 {
+                    var statusError = NSError(domain:"com.raywenderlich", code:httpResponse.statusCode, userInfo:[NSLocalizedDescriptionKey : "HTTP status code has unexpected value."])
+                    //completion(data: nil, error: statusError)
+                } else {
+                   // completion(data: data, error: nil)
+                }
+            }
+            
+        })
+        loadDataTask.resume()*/
+        
+        data = NSData(contentsOfURL: NSURL(string: GMSURL+address.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!)!)
+        
+        println("URL="+GMSURL+address.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!)
+        
+        let json = JSON(data:data)
+        if let lat = json["results"][0]["geometry"]["location"]["lat"].double {
+            latitude=lat
+        }
+        if let lng = json["results"][0]["geometry"]["location"]["lng"].double {
+            longtitude=lng
+        }
+        if let addr = json["results"][0]["formatted_address"].string{
+            formatted_address=addr
+        }
+        println("latitude=\(latitude) , longtitude=\(longtitude)")
+        println("address=\(formatted_address)")
+        return CLLocationCoordinate2D(latitude: latitude, longitude: longtitude);
+        
+        
+
+       // return CLLocationCoordinate2D(latitude: latitude, longitude: longtitude);
+    }
+}
--- Map Tagger/ViewController.swift
+++ Map Tagger/ViewController.swift
@@ -7,18 +7,105 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+class MarkerInfo: NSObject {
+    var coordinate: CLLocationCoordinate2D
+    var address: String
+    init(coordinate : CLLocationCoordinate2D,address : String){
+        self.coordinate = coordinate
+        self.address = address
+        super.init()
+    }
+}
+class ViewController: UIViewController , CLLocationManagerDelegate{
     @IBOutlet weak var mapView: GMSMapView!
+    @IBOutlet weak var addressTextField: UITextField!
 
+    @IBAction func searchAddressTapped(sender: AnyObject) {
+        println(addressTextField.text);
+
+        
+        /* DataManager.getCoordinateFromGMS(addressTextField.text) { (GMSData) -> CLLocationCoordinate2D in
+            
+           let json = JSON(data: GMSData)
+            if let lat = json["results"][0]["geometry"]["location"]["lat"].double {
+                print("\(lat),")
+                latitude=lat
+            }
+            if let lng = json["results"][0]["geometry"]["location"]["lng"].double {
+                println("\(lng)")
+                longtitude=lng
+            }
+            
+            println("inDATA:\(latitude),\(longtitude)")
+
+
+        }*/
+        var coordinate:CLLocationCoordinate2D!
+        coordinate=DataManager.getCoordinateFromGMS(addressTextField.text)
+        //self.mapView.camera = GMSCameraPosition(target: CLLocationCoordinate2D(latitude: 35.6894875 , longitude: 139.6917064 ) , zoom: 15, bearing: 0, viewingAngle: 0)
+        mapView.camera = GMSCameraPosition(target: coordinate , zoom: 10, bearing: 0, viewingAngle: 0)
+        mapView.clear()
+        var marker = GMSMarker(position: coordinate)
+        marker.title="Hello World"
+        marker.map=self.mapView
+        marker.snippet="\(coordinate.latitude),\(coordinate.longitude)"
+    
+        //print("wiser:\(latitude)\(longtitude)")
+        // mapView.camera = GMSCameraPosition(target: CLLocationCoordinate2D(latitude: latitude , longitude: longtitude ) , zoom: 15, bearing: 0, viewingAngle: 0)
+        // 6 CLLocationCoordinate2D
+
+    }
+    @IBAction func mapTypeSegmentPressed(sender: AnyObject) {
+        let segmentedControl = sender as UISegmentedControl
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = kGMSTypeNormal
+        case 1:
+            mapView.mapType = kGMSTypeSatellite
+        case 2:
+            mapView.mapType = kGMSTypeHybrid
+        default:
+            mapView.mapType = mapView.mapType
+        }
+    }
+    let locationManager = CLLocationManager()
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        // 2
+        if status == .AuthorizedWhenInUse {
+            
+            // 3
+            locationManager.startUpdatingLocation()
+            
+            //4
+            mapView.myLocationEnabled = true
+            mapView.settings.myLocationButton = true
+        }
+    }
+    
+    // 5
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        if let location = locations.first as? CLLocation {
+            
+            // 6 CLLocationCoordinate2D
+            mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
+            
+            // 7
+            locationManager.stopUpdatingLocation()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
 
 
